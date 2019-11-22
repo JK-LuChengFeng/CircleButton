de16a40c9f74dc5847124f70dd97082544a52117
JK <1030088968@qq.com>
2019-11-22 14:23:25 +0800

    Auto Description

--- app/src/main/java/com/example/circlebutton/Main4Activity.java
+++ app/src/main/java/com/example/circlebutton/Main4Activity.java
@@ -3,15 +3,12 @@ package com.example.circlebutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class Main3Activity extends AppCompatActivity {
-
-    private TextView tv_cheshi;
+public class Main4Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main4);
     }
 }
